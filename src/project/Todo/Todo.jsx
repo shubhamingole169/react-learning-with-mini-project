e765351b95542838f42891531fa570a6dcd23baf
Shubham Ingole <shubhamingole169@gmail.com>
2025-01-28 17:34:35 +0530

    date and time has section added git status

--- src/project/Todo/Todo.jsx
+++ src/project/Todo/Todo.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
 import "./Todo.css"
 import { MdCheck, MdDeleteForever } from "react-icons/md";
 export const Todo = () => {
     const [inputValue, setInputValue] = useState("");
     const [task, setTask] = useState([]);
+    const [ dateTime, setDateTime] = useState("");
 
     const handleInputChange = (value) =>{
         setInputValue(value);
@@ -17,16 +17,35 @@ export const Todo = () => {
         if(task.includes(inputValue)){
             setInputValue("");
             return;
-        } 
+        }
 
         setTask((prevTask) =>[...prevTask, inputValue]);
 
         setInputValue("");
-    }
+    };
+
+    // todo data and time
+
+    useEffect(() => {
+    const interval =  setInterval(() =>{
+            const now = new Date();
+            const formattedDate = now.toLocaleDateString();
+            const formattedTime = now.toLocaleTimeString();
+
+        
+            setDateTime(`${formattedDate} - ${formattedTime}`);
+            }, 1000);
+
+        return () => clearInterval(interval);
+        
+    })
+
+
     return (
         <section className="todo-container">
             <header>
                 <h1>Todo List</h1>
+                <h2 className="date-time"> {dateTime} </h2>
             </header>
             <section className="form">
                 <form onSubmit={handleSubmitForm}>
