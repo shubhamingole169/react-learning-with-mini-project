93d6ef69bb9f8f8fd12d04d4052ec8961c6d018b
Shubham Ingole <shubhamingole169@gmail.com>
2025-01-28 12:31:34 +0530

    react icon added

--- src/project/ToggleSwitch/ToggleSwitch.jsx
+++ src/project/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "./ToggleSwitch.css"
+import { IoIosSwitch } from "react-icons/io";
 export const ToggleSwitch = () =>{
     const [isOn, setIsOn] = useState(false);
 
@@ -8,10 +9,13 @@ export const ToggleSwitch = () =>{
     };
     const checkIsOn = isOn ? "on" : "off"
     return(
+        <>
+        <h1 style={{color:"000", textAlign:"center"}}> Toggle Switch <IoIosSwitch /> </h1>
         <div className="toggle-switch" onClick={handleToggleSwitch}>
         <div className= {`switch ${checkIsOn}`}>
             <span className="switch-state"> {checkIsOn} </span>
         </div>
         </div>
+        </>
     )
 }
