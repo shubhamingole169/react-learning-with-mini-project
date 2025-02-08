c6a71b102a65904a928e4bf1c096eb15c9cad95b
Shubham Ingole <shubhamingole169@gmail.com>
2025-02-08 16:03:05 +0530

    useMemoHook is remained as it is git add . not understand

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ReactMemo } from "./components/hooks/Memo/ReactMemo";
-
+import "./index.css"
 
 
 
---
+++ src/components/Hooks/Memo/UseMemo.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const ExpensiveComponent = () =>{
+    
+    //   Expensive Calculation function
+    const sum = () =>{
+        console.log("Calculating sum ...");
+
+        let i = 0;
+        for(i = 0; i<= 1000000000; i++){
+            i = i + 1;
+        }
+        return i;
+    }
+
+    const total = sum();
+
+    return <p> sum : {total} </p>
+};
+
+
+const MemoParentComponent = () =>{
+    const [count, setCount] = useState(0);
+    return(
+        <div className="p-4 h-lvh font-display tracking-wider flex flex-col justify-col justify-center items-center bg-black text-white">
+            <ExpensiveComponent />
+            <button
+            onClick={() => setCount(count + 1)}
+            className="py-3 px-6 bg-cyan-400 rounded-sm"
+            >
+                Re-render Parent
+            </button>
+            <p>parent re-renders:{count}</p>
+        </div>
+    )
+}
