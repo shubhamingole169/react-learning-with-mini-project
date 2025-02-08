25e0d30af59b256420fbe3eca2a3a38b15c3e48b
Shubham Ingole <shubhamingole169@gmail.com>
2025-02-08 09:59:28 +0530

    useReduced learned git add .

--- src/components/Hooks/useReducer/index.jsx
+++ src/components/Hooks/useReducer/index.jsx
@@ -1,23 +1,21 @@
-import { useState } from "react";
 import { useReducer } from "react";
 
 
 export const ReducerComp = () =>{
-
+    
     const reducer = (state, action) =>{
-        console.log(state, action);
-        switch (type) {
-            case INCREMENT:
-                
-                break;
-        
-            default:
-                break;
+        console.log("state :", state);
+        console.log("action:",action);
+        if (action.type === "INCREMENT") {
+            return state + 1;
         }
+        if (action.type === "DECREMENT") {
+            return state - 1;
+        }
+    };
 
-    }
-    const [count, dispatch] = useReducer(ReducerComp, 0);
-    console.log(useReducer(reducer, 0));
+
+    const [count, dispatch] = useReducer(reducer, 5);
     return <>
         <div className=" p-4 h-lvh flex flex-col justify justify-center ">
             <h1>{count}</h1>
